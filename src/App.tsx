a1e56ab03bb9f845cc7b22c431c77b4eec340cca
Prajwal Sharma <prjwl4@gmail.com>
2023-05-31 00:06:16 +0530

    test: rebuild to replace env variable

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <strong>Environment Variable:</strong> {import.meta.env.VITE_SOME_KEY}
+      <strong>Environment Variable :</strong> {import.meta.env.VITE_SOME_KEY}
     </div>
   );
 }
